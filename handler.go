2dc2a70a27bc4e8c24534e885489e8153f579268
Samuel Berthe <dev@samuel-berthe.fr>
2024-07-10 15:35:41 +0200

    feat: when the name is empty, WithGroup returns the receiver

--- handler.go
+++ handler.go
@@ -81,6 +81,11 @@ func (h *LokiHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
 }
 
 func (h *LokiHandler) WithGroup(name string) slog.Handler {
+	// https://cs.opensource.google/go/x/exp/+/46b07846:slog/handler.go;l=247
+	if name == "" {
+		return h
+	}
+
 	return &LokiHandler{
 		option: h.option,
 		attrs:  h.attrs,
