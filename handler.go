8bd666efd2f4d0b256b21b549d34de7f877fdab6
Andres Eloy <andres.epacheco99@gmail.com>
2025-09-30 21:31:35 -0400

    refactor: LokiHandler.Handle method delegates record modification to the Converter

--- handler.go
+++ handler.go
@@ -27,24 +27,26 @@ type Option struct {
 	AddSource   bool
 	ReplaceAttr func(groups []string, a slog.Attr) slog.Attr
 
+	// When set to true, this handler sends record attributes as structured metadata. To send all record attributes as structured metadata,
+	// use the RemoveAttrsConverter along with this option set to true.
+	//
 	// In Loki, labels are key-value pairs used to index and filter log streams.
-	// They enable powerful querying but must remain low in cardinality to maintain performance.
+	// They enable powerful querying but must remain in low cardinality to maintain performance.
 	//
 	// By default, LokiHandler.Handle sends all log record attributes as labels to Loki.
-	// This works well for common, low-cardinality fields like service name, log level, or environment.
+	// This works well for common, low-cardinality fields like service name, log level, environment, etc.
 	//
-	// However, in some cases, you may want to include high-cardinality metadata—such as request IDs, user IDs, or session tokens—for improved debugging and traceability.
+	// However, in some cases, you may want to include high-cardinality metadata, such as request IDs, user IDs, or session tokens, for improved debugging and traceability.
 	//
-	// Starting with schema version 13, Loki introduced structured metadata support, allowing high-cardinality attributes to be attached to log records without indexing them as labels.
-	// This helps preserve Loki’s performance while still making the data available in the logs.
+	// Starting with schema version 13, Loki introduced structured metadata, allowing high-cardinality attributes to be attached to log records without indexation.
+	// This helps preserve Loki’s performance while improving traceability and logging capabilities.
 	//
-	// When set to true, this handler sends record attributes as structured metadata instead of labels.
-	// Attributes added via LokiHandler.WithAttrs are always sent as labels, regardless of this setting.
-	// If set to false (default), the handler treats all record attributes as labels.
+	// Note: Attributes added via LokiHandler.WithAttrs are always sent as labels, regardless of the value of this setting.
+	// If set to false (default), the handler will not send record attributes as structured metadata.
 	//
 	// Learn more about structured metadata in Loki:
 	// https://grafana.com/docs/loki/latest/get-started/labels/structured-metadata/
-	HandleRecordAttrsAsStructuredMetadata bool
+	HandleRecordsWithMetadata bool
 }
 
 // Creating a Loki client at each `NewLokiHandler` call may lead to connection
@@ -88,19 +90,9 @@ func (h *LokiHandler) Enabled(_ context.Context, level slog.Level) bool {
 func (h *LokiHandler) Handle(ctx context.Context, record slog.Record) error {
 	fromContext := slogcommon.ContextExtractor(ctx, h.option.AttrFromContext)
 
-	if h.option.HandleRecordAttrsAsStructuredMetadata {
-		// if structured metadata is enabled, we need to strip the record from its attributes to avoid duplication
-		// in the model.LabelSet and structured metadata fields. This ensures the result does not depend on
-		// the Converter implementation.
-		strippedRecord := slog.Record{
-			Time:    record.Time,
-			Level:   record.Level,
-			Message: record.Message,
-			PC:      record.PC,
-		}
+	attrs := h.option.Converter(h.option.AddSource, h.option.ReplaceAttr, append(h.attrs, fromContext...), h.groups, &record)
 
-		attrs := h.option.Converter(h.option.AddSource, h.option.ReplaceAttr, append(h.attrs, fromContext...), h.groups, &strippedRecord)
-
+	if h.option.HandleRecordsWithMetadata {
 		var m push.LabelsAdapter
 		record.Attrs(func(attr slog.Attr) bool {
 			m = append(m, push.LabelAdapter{
@@ -111,8 +103,6 @@ func (h *LokiHandler) Handle(ctx context.Context, record slog.Record) error {
 		})
 		return h.option.Client.HandleWithMetadata(attrs, record.Time, record.Message, m)
 	}
-
-	attrs := h.option.Converter(h.option.AddSource, h.option.ReplaceAttr, append(h.attrs, fromContext...), h.groups, &record)
 
 	return h.option.Client.Handle(attrs, record.Time, record.Message)
 }
